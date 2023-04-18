44084291ca370b030c397df0bb435ae11716347e
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-04-18 09:31:53 +0300

    Create HomePage.test.js

---
+++ src/__test__/HomePage.test.js
@@ -0,0 +1,24 @@
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import React from 'react';
+import Home from '../pages/Home';
+import store from '../redux/Store';
+import '@testing-library/jest-dom/extend-expect';
+
+window.scrollTo = jest.fn();
+
+describe('Details Component', () => {
+  it('should render the details correctly', async () => {
+    window.scrollTo.mockClear();
+    const component = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Home />
+        </Provider>
+        ,
+      </BrowserRouter>,
+    );
+    expect(component.asFragment()).toMatchSnapshot();
+  });
+});
